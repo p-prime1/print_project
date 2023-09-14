232e79d9152fa11f4f327bec2c9195d2f753819c
p-prime1 <preciousnwaka95@gmail.com>
2023-09-14 19:40:28 +0100

    Prints a short description of myself

---
+++ Playground_Cpp/strings.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string whoami;
+	
+	whoami = "I am a mystery even to myself, i asked myself several times what do i even want. I hope to find out oneday Jehovahs willing.But meanwhile am working on making myself a better son, a better brother, a better friend,a better husband lol";
+	cout << whoami << endl;
+	return (0);
+}
