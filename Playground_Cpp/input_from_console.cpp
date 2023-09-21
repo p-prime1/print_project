8082cf54e2e2c0482be8258954136c4defd9d7ed
p-prime1 <preciousnwaka95@gmail.com>
2023-09-21 20:54:56 +0100

    Added new file

---
+++ Playground_Cpp/input_from_console.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string name = " ";
+
+	cout << "What do we call you?\n";
+	cin >> name;
+	cout << "Hello" << " " << name << "!\n";
+
+	return (0);
+}
